d589a7a9f7f9397d324398c51155018bc66efaae
SeungHyeonOh <postercolo@naver.com>
2015-10-22 17:37:27 +0900

    [2015.10.22] 교재로 상속을 조금 더 공부함

--- JAVA/src/com/javalec/ex07/_17_CarTest.java
+++ JAVA/src/com/javalec/ex07/_17_CarTest.java
@@ -10,7 +10,8 @@ class Car {
 	int price;
 
 	Car() {
-		this("white", "auto", 4);
+//		this("white", "auto", 4);
+		this("벤츠", "2016", "Germany", 150000);
 	}
 
 	Car(String c, String g, int d) {
@@ -40,7 +41,7 @@ public class _17_CarTest {
 		System.out.println("c1의 color=" + c1.color + ", gearType=" + c1.gearType + ", door=" + c1.door);
 		System.out.println("c2의 color=" + c2.color + ", gearType=" + c2.gearType + ", door=" + c2.door);
 
-		Car c3 = new Car("벤츠", "2016", "Germany", 150000);
+		Car c3 = new Car();
 		System.out.println(" 브랜드 : " + c3.brand + "\n 생산년도 : " + c3.madeYear + "\n 생산국 : " + c3.madeIn + "\n 가격 : " + c3.price);
 
 	}
---
+++ 뇌를 자극하는 Java Programming/src/com/brain/ch6/BonusPointAccount.java
@@ -0,0 +1,17 @@
+package com.brain.ch6;
+
+public class BonusPointAccount extends Account {
+	int bonusPoint;
+
+	BonusPointAccount(String accountNo, String ownerName, int balance, int bonusPoint) {
+		super(accountNo, ownerName, balance);
+		this.bonusPoint = bonusPoint;
+	}
+
+	void deposit(int amount) {
+		super.deposit(amount);
+		// balance += amount;
+		bonusPoint += (int) (amount * 0.001);
+	}
+
+}
